80a81faa325fd84ddcc8d871a22919e0a36a889e
Marcin <marcin.sztylko@gmail.com>
2019-08-26 20:34:42 +0200

    change resolution function

--- MeAtWork.py
+++ MeAtWork.py
@@ -3,15 +3,26 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+change_res(cap, 640, 480)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
+    out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
