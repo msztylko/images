b7419e00c73ca31f321e901d5d86b88f93daa508
Marcin <marcin.sztylko@gmail.com>
2019-08-27 11:11:53 +0200

    version for saving videos

--- MeAtWork.py
+++ MeAtWork.py
@@ -3,15 +3,16 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+# Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output1.avi',fourcc, 20.0, (480, 640))
+out = cv2.VideoWriter('me-at-work.avi', fourcc, 20.0, (640, 480))
 
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-#    out.write(frame)
+    cv2.imshow('frame', frame)
+    out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
