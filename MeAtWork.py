24fffbc00659df19f8c9f0fe8f496070f8eaa9a8
Marcin <marcin.sztylko@gmail.com>
2019-08-27 10:57:25 +0200

    remove change_res, let's stick to 480p

--- MeAtWork.py
+++ MeAtWork.py
@@ -3,22 +3,15 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
-
-
-change_res(cap, 1280, 720)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output1.avi',fourcc, 20.0, (1280, 720))
+out = cv2.VideoWriter('output1.avi',fourcc, 20.0, (480, 640))
 
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    out.write(frame)
+#    out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
